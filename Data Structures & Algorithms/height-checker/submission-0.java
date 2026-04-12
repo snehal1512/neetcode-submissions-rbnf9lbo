591fdaefcfc0f3d66d16077217896a663650fecc
snehal1512 <69391836+snehal1512@users.noreply.github.com>
2026-04-12 11:35:23 -0500

    Add: height-checker - submission-0

---
+++ Data Structures & Algorithms/height-checker/submission-0.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+
+        int count = 0;
+        
+        int[] arr = new int[heights.length];
+
+        for(int i=0;i<heights.length;i++){
+            arr[i] = heights[i];
+        }
+
+        Arrays.sort(arr);
+
+        for(int i=0;i<heights.length;i++){
+            if(arr[i]!=heights[i]){
+                count++;
+            }
+        }
+
+        return count;
+    }
+}

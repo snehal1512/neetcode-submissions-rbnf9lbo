301f69ba26982f367ef5d9e0e4fe783733243dea
snehal1512 <69391836+snehal1512@users.noreply.github.com>
2026-05-09 09:30:05 -0500

    Add: first-unique-character-in-a-string - submission-2

---
+++ Data Structures & Algorithms/first-unique-character-in-a-string/submission-2.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int firstUniqChar(String s) {
+
+        if(s.length() == 0) return -1;
+        
+        Map<Character,Integer> map = new HashMap<>();
+
+        for(char c: s.toCharArray()){
+            map.put(c, map.getOrDefault(c,0)+1);
+        }
+
+        for(int i=0;i<s.length();i++){
+
+            if(map.get(s.charAt(i)) == 1){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
